4d7d751f82562ad2b24816f985d5899716692bb4
Cody Bontecou <bontecouc@gmail.com>
2020-11-18 15:56:13 -0800

    cursor pointers on botom nav

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -2,12 +2,12 @@
   <div
     class="relative w-full h-66 px-8 py-4 flex justify-between rounded-full bg-black"
   >
-    <PersonOutline />
-    <SearchIcon />
-    <BulletedList />
+    <PersonOutline class="cursor-pointer" />
+    <SearchIcon class="cursor-pointer" />
+    <BulletedList class="cursor-pointer" />
     <div></div>
     <BaseCircle
-      class="absolute right-0 top-0 w-66 h-66"
+      class="absolute cursor-pointer right-0 top-0 w-66 h-66"
       color="gradient-black-green"
     >
       <LightingFillIcon />
