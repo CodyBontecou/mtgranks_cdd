1e1244754dd959d363055a986c009ff35559f093
Cody Bontecou <bontecouc@gmail.com>
2020-11-26 20:35:58 -0800

    removed components from purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,6 @@ module.exports = {
     enabled: process.env.NODE_ENV === 'production',
     // enabled: false,
     content: [
-      // 'components/**/*.vue',
       'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
