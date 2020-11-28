471e58542ce788c615b6321f452de800bc226b10
Cody Bontecou <bontecouc@gmail.com>
2020-11-27 19:22:27 -0800

    Pretty sure I have gtag (Google Analytics 4) setup. Just needs to be under mtgranks.com to work. Close #13

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,7 +94,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ['~/plugins/gtag.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
---
+++ plugins/gtag.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-E1E1D4GWF6' },
+})
