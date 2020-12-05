a063baa5577cd18ab9eca9c468aebad40b37bff5
Cody Bontecou <bontecouc@gmail.com>
2020-12-05 11:45:54 -0800

    Removed code that occurred in dev mode. Only utilizing asyncdata now.

--- pages/_set/_card/index.vue
+++ pages/_set/_card/index.vue
@@ -16,7 +16,7 @@
 <script>
 export default {
   layout: 'wideHeader',
-  async asyncData({ payload, params, $axios }) {
+  asyncData({ payload }) {
     if (payload) {
       return {
         card: payload.card,
@@ -24,93 +24,6 @@ export default {
         set: payload.set,
         sets: payload.sets,
       }
-    } else {
-      const sets = [
-        {
-          name: 'Zendikar Rising',
-          code: 'ZNR',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/znr.svg',
-          cardCount: '220',
-          color: 'black-gold',
-          type: 'new',
-          slug: 'zendikar-rising',
-        },
-        {
-          name: 'Ikoria: Lair of Behemoths',
-          code: 'IKO',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/iko.svg',
-          cardCount: '264',
-          color: 'orange-red',
-          type: 'new',
-          slug: 'ikoria-lair-of-behemoths',
-        },
-        {
-          name: 'Core 2021',
-          code: 'M21',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/m21.svg',
-          cardCount: '264',
-          color: 'black-green',
-          type: 'new',
-          slug: 'core-2021',
-        },
-        {
-          name: 'Theros Beyond Death',
-          code: 'THB',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/thb.svg',
-          cardCount: '220',
-          color: 'ash',
-          type: 'old',
-          slug: 'theros-beyond-death',
-        },
-        {
-          name: 'Throne of Eldraine',
-          code: 'ELD',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/eld.svg',
-          cardCount: '264',
-          color: 'teal',
-          type: 'old',
-          slug: 'throne-of-eldraine',
-        },
-        {
-          name: 'Core 2020',
-          code: 'M20',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/m20.svg',
-          cardCount: '264',
-          color: 'mandarin',
-          type: 'old',
-          slug: 'core-2020',
-        },
-        {
-          name: 'War of the Spark',
-          code: 'WAR',
-          icon: 'https://c2.scryfall.com/file/scryfall-symbols/sets/war.svg',
-          cardCount: '264',
-          color: 'peach',
-          type: 'old',
-          slug: 'war-of-the-spark',
-        },
-      ]
-      const set = sets.find((set) => set.slug === params.set)
-      const { data } = await $axios.get(
-        `https://api.scryfall.com/cards/search?q=set:${set.code}+is:booster`
-      )
-      data.data.forEach((card) => {
-        card.slug = card.name
-          .replace(/[/:.,']/g, '')
-          .replace(/ /g, '-')
-          .toLowerCase()
-
-        for (const uri in card.purchase_uris) {
-          card.purchase_uris[uri] = card.purchase_uris[uri].replace(
-            'utm_source=scryfall',
-            'utm_source=mtgranks'
-          )
-        }
-      })
-      const cards = data.data
-      const card = cards.find((card) => card.slug === params.card)
-
-      return { card, cards, set, sets }
     }
   },
   computed: {
