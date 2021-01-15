48e588a97b103e5ff203ea69f77b50797a66c060
Cody Bontecou <bontecouc@gmail.com>
2021-01-14 18:18:34 -1000

    log for axios response

--- components/SubscriptionButton.vue
+++ components/SubscriptionButton.vue
@@ -39,6 +39,7 @@ export default {
             }
           )
           .then((link) => {
+            console.log(link)
             window.location.href = link
           })
           .catch((err) => console.error(err))
