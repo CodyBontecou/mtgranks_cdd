37ee66431028357f5a5fc79325379d924f3c0f24
Cody Bontecou <bontecouc@gmail.com>
2021-02-02 12:16:48 -1000

    login logic working again

--- components/Login.vue
+++ components/Login.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="fixed mr-20 right-0 cursor-pointer">
-    <User
-      v-if="isLoggedIn"
-      @click.native="triggerNetlifyIdentityAction('open')"
-    />
-    <button
-      v-else
-      class="font-medium bg-white rounded p-2 items-center hover:shadow-2xl border border-ash border-opacity-25"
-      @click="triggerNetlifyIdentityAction('login')"
-    >
-      <span class="opacity-75">Sign In</span>
-    </button>
+  <div
+    class="fixed bg-white mr-20 right-0 cursor-pointer p-1 rounded-full shadow-2xl hover:shadow-lg border border-opacity-25"
+    :class="{
+      'border-ash text-ash': !isLoggedIn,
+      'border-blue-400 text-blue-400': isLoggedIn,
+    }"
+  >
+    <User @click.native="triggerNetlifyIdentityAction('open')" />
+    <!--    <button-->
+    <!--      v-else-->
+    <!--      class="text-black font-medium bg-white rounded p-2 items-center hover:shadow-2xl border border-ash border-opacity-25"-->
+    <!--      @click="triggerNetlifyIdentityAction('login')"-->
+    <!--    >-->
+    <!--      <span class="opacity-75">Sign In</span>-->
+    <!--    </button>-->
   </div>
 </template>
 
@@ -29,13 +32,19 @@ export default {
       setUser: 'user/setUser',
     }),
     triggerNetlifyIdentityAction(action) {
+      window.netlifyIdentity.on('signup', (user) => {
+        console.log(user)
+        this.setUser(user)
+        window.netlifyIdentity.close()
+      })
+      window.netlifyIdentity.on('login', (user) => {
+        console.log(user)
+        this.setUser(user)
+        window.netlifyIdentity.close()
+      })
       try {
         if (action === 'login' || action === 'signup') {
           window.netlifyIdentity.open(action)
-          window.netlifyIdentity.on(action, (user) => {
-            this.setUser(user)
-            window.netlifyIdentity.close()
-          })
         } else if (action === 'open') {
           window.netlifyIdentity.open()
           window.netlifyIdentity.on('logout', () => {
