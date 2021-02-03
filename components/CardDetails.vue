3d67791a90e7c06f0dbe349b4654b34c9e34d151
Cody Bontecou <bontecouc@gmail.com>
2021-02-02 20:11:43 -1000

    new components for previous UI changes

---
+++ components/CardDetails.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="card" class="z-40 flex flex-col order-2 justify-center">
+    <CardImg :card="card" />
+    <p class="mx-20 text-sm text-white leading-160 font-medium">
+      {{ rating.reason }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'CardDetails',
+  data() {
+    return {
+      rating: {
+        rator: {
+          name: 'Justlolaman',
+        },
+        rating: 'A-',
+        reason:
+          'Best cards in the format. Bombs. Completely changes the game in your\n' +
+          '      favor. Will almost single handedly win you the game.',
+      },
+    }
+  },
+  computed: {
+    ...mapGetters({
+      expanded: 'expanded',
+      card: 'card',
+    }),
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ components/ToggleCardImage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    v-if="card.card_faces"
+    class="absolute rounded-full bg-white opacity-100 cursor-pointer p-2 top-0 left-0"
+    :class="{
+      'top-10 left-10': expanded,
+    }"
+    @click="toggleFace"
+  >
+    <SwitchVertical
+      class="h-2 w-2 text-black fill-current md:h-5 md:w-5"
+      :class="{
+        'h-5 w-5': expanded,
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ToggleCardImage',
+  data() {
+    return {
+      face: 0,
+    }
+  },
+  computed: {
+    ...mapGetters(['card', 'expanded']),
+    cardFace() {
+      return this.card.card_faces[this.face].image_uris.border_crop
+    },
+  },
+  methods: {
+    toggleFace() {
+      if (this.face === 0) {
+        this.face = 1
+      } else {
+        this.face = 0
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
